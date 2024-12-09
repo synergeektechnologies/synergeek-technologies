7e1d2587aab4baaa8c737a0059a2e13e2253e3f4
srimanikandanr <srimanikandandev@gmail.com>
2024-12-09 20:30:56 +0530

    updating sitemap config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    sitemap({
+      hostname: 'https://synergeektechnologies.in',
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
